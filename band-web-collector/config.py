74ded52cf25ee6762b3338762ca26f0a3aaf1e74
xwovlflx-ui <xwovlflx@gmail.com>
2026-06-08 22:41:29 +0900

    Add BAND web collector config

---
+++ band-web-collector/config.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+START_URL = 'https://band.us'
+OUTPUT_DIR = Path('band_web_output')
+SCREENSHOT_DIR = OUTPUT_DIR / 'screenshots'
+LOG_DIR = OUTPUT_DIR / 'logs'
+EXCEL_PATH = OUTPUT_DIR / 'band_posts.xlsx'
+SCROLL_COUNT = 30
+SCROLL_WAIT_SECONDS = 2
